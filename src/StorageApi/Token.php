5ea3420f050c1db56f201ab0fa8df5c76292599d
Erik Zigo <erik.zigo@keboola.com>
2018-05-15 15:53:36 +0200

    Token class

---
+++ src/StorageApi/Token.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\ProjectRestore\StorageApi;
+
+use Keboola\StorageApi\Client;
+
+class Token
+{
+    /**
+     * @var array
+     */
+    private $tokenData;
+
+    /**
+     * @var string
+     */
+    private $token;
+
+    public function __construct(Client $sapiClient)
+    {
+        $this->tokenData = $sapiClient->verifyToken();
+        $this->token = $sapiClient->token;
+    }
+
+    /**
+     * Convert to SAPI Token
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->token;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->tokenData['description'];
+    }
+
+    public function getProjectId(): int
+    {
+        return $this->tokenData['owner']['id'];
+    }
+
+    public function getProjectName(): string
+    {
+        return $this->tokenData['owner']['name'];
+    }
+
+    public function hasProjectMysqlBackend(): bool
+    {
+        return isset($this->tokenData["owner"]["hasMysql"]) && $this->tokenData["owner"]["hasMysql"] === true;
+    }
+
+    public function hasProjectRedshiftBackend(): bool
+    {
+        return isset($this->tokenData["owner"]["hasRedshift"]) && $this->tokenData["owner"]["hasRedshift"] === true;
+    }
+
+    public function hasProjectSnowflakeBackend(): bool
+    {
+        return isset($this->tokenData["owner"]["hasSnowflake"]) && $this->tokenData["owner"]["hasSnowflake"] === true;
+    }
+
+}
